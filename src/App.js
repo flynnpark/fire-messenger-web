f66d3ebc84e63092c374b30b7c88c87cc66482e1
Flynn <wphestiraid@gmail.com>
2018-01-29 16:10:00 +0900

    NODE_PATH 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import styles from './App.scss';
+import styles from 'App.scss';
 
 class App extends Component {
     render() {
         return (
             <div className={styles.App}>
+                App
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import store from "redux/configureStore";
+import App from "App";
+import registerServiceWorker from 'registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
