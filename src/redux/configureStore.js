fcd5abe5244ac528e931368bd9f9a3743e7f23aa
Flynn <wphestiraid@gmail.com>
2018-01-30 09:59:34 +0900

    Redux Dev Tool 설치

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
 import user from "redux/modules/user";
 
 const env = process.env.NODE_ENV;
@@ -20,7 +21,15 @@ const reducer = combineReducers({
     routing: routerReducer
 });
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+let store;
+if (env === "development") {
+    store = initialState => createStore(
+        reducer,
+        composeWithDevTools(applyMiddleware(...middlewares))
+    );
+} else {
+    store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
 
 export { history };
 
