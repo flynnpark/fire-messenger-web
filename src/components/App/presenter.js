242a10b9455b9bdcc35ddb6c56af028e5efd5377
Flynn <wphestiraid@gmail.com>
2018-01-30 13:36:43 +0900

    Auth 컴포넌트 파트 1

--- src/components/App/presenter.js
+++ src/components/App/presenter.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
 import styles from "./styles.scss";
+import Auth from "components/Auth";
 
 const App = props => [
     props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />
 ];
+
+App.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired,
+}
 
 const PrivateRoutes = props => (
     <Switch>
@@ -14,7 +20,7 @@ const PrivateRoutes = props => (
 
 const PublicRoutes = props => (
     <Switch>
-        <Route exact path="/" render={() => "Log in"} />
+        <Route exact path="/" component={Auth} />
     </Switch>
 );
 
---
+++ src/components/Auth/presenter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Auth = props => <h1>Auth component</h1>;
+
+export default Auth;
